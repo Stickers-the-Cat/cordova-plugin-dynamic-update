0445aeb520cf94df8d9f54a1d5b17b45117db4e1
Leslie Bruder <lescold@gmail.com>
2017-06-23 01:40:12 -0400

    BLAAAAAAAAH@$%$#^$%&^*^$%&$%&

--- src/android/DynamicUpdate.java
+++ src/android/DynamicUpdate.java
@@ -78,7 +78,7 @@ public class DynamicUpdate extends CordovaPlugin {
 
 			} catch (Exception e) {
 
-				PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getMassage);
+				PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getMessage());
 				callback.sendPluginResult(result);
 				return false;
 			}
